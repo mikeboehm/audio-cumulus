ae5973b987b5700b150da3d35f3c28190e279c20
hammy <hammy@limesharp.net>
2014-11-26 14:26:57 +0000

    adding basic ordering to track list

--- static/app.js
+++ static/app.js
@@ -5,10 +5,14 @@
     app.controller('StreamController', [ '$http' , function( $http ){  
         var stream = this;
         stream.tracks = [];
+        this.pagination = 'title';
         $http.get('/get_tracks').success(function(data){
             stream.tracks = data.tracks;
         });
 
+        this.updatePagination = function(new_order){
+            this.pagination = '-' + new_order;
+        }
         this.play = function(stream_url){
             $http.post('/play_stream', { url : stream_url});
         }
