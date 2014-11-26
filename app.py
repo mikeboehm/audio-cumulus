a6d13b6b8f20efccd27d1a3f312536a16d10cf2e
Mike Boehm <mike@boehm.com.au>
2014-11-26 22:56:47 +0000

    Squash commits

--- app.py
+++ app.py
@@ -6,15 +6,28 @@ import json
 import time
 import os
 import xml.etree.ElementTree as ET
+from connection import ScDetails
 
-client = soundcloud.Client(client_id='xxx', client_secret='xxx', username='xxx', password='xxx')
+details = ScDetails();
+client = soundcloud.Client(client_id=details.client_id, client_secret=details.client_secret, username=details.username, password=details.password)
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-	return render_template('index.html')
+	return render_template('index.html', home=True)
 
+@app.route("/stream")
+def stream():
+	return render_template('stream.html', stream=True)
+
+@app.route("/favourites")
+def favourites():
+	return render_template('favourites.html', favourites=True)
+
+@app.route("/others_favourites")
+def others_favourites():
+	return render_template('others_favourites.html', otherfavourites=True)
 
 @app.route("/play", methods=['GET', 'POST'])
 def play():
@@ -39,8 +52,8 @@ def play_stream():
 @app.route("/get_tracks")
 def get_tracks():
 	# if current file exists and is less than a day old
-	if os.path.isfile('list.txt') and os.path.getmtime('list.txt') > (time.time() - 86400):
-		with open('list.txt') as data:
+	if os.path.isfile('data/list.txt') and os.path.getmtime('data/list.txt') > (time.time() - 86400):
+		with open('data/list.txt') as data:
 			return jsonify(json.load(data))
 
 	# otherwise regenerate the file
@@ -59,17 +72,78 @@ def get_tracks():
 						stream_url=track.stream_url,
 						user=track.user['username'],
 						date=track.created_at,
-
+						permalink=track.permalink_url
 					))
-		with open('list.txt', 'w') as outfile:
+		with open('data/list.txt', 'w') as outfile:
 			json.dump({'tracks' : tracks}, outfile)
 		return jsonify({ 'tracks' : tracks })
 
-@app.route('/embedcode/', methods=['GET'])
+@app.route("/get_favourites")
+def get_favourites():
+	# if current file exists and is less than a day old
+	if os.path.isfile('data/favourites.txt') and os.path.getmtime('data/favourites.txt') > (time.time() - 86400):
+		with open('data/favourites.txt') as data:
+			return jsonify(json.load(data))
+
+	# otherwise regenerate the file
+	else:
+		me = client.get('/me/')
+		fav_tracks = client.get('/users/' + str(me.id) + '/favorites')
+		tracks = []
+		for track in fav_tracks:
+			if hasattr(track, 'stream_url'):
+				tracks.append(dict(
+					id=track.id,
+					duration=track.duration,
+					title=track.title,
+					stream_url=track.stream_url,
+					user=track.user['username'],
+					date=track.created_at,
+					permalink=track.permalink_url
+				))
+		with open('data/favourites.txt', 'w') as outfile:
+			json.dump({'tracks' : tracks}, outfile)
+		return jsonify({ 'tracks' : tracks })
+
+
+@app.route("/get_others_favourites")
+def get_others_favourites():
+	# if current file exists and is less than a day old
+	if os.path.isfile('data/others_favourites.txt') and os.path.getmtime('data/others_favourites.txt') > (time.time() - 86400):
+		with open('data/others_favourites.txt') as data:
+			return jsonify(json.load(data))
+
+	# otherwise regenerate the file
+	else:
+		followers = client.get('/me/followings')
+		users = [dict(id=follow.id, username=follow.username) for follow in followers]
+		tracks = []
+		for user in users:
+			user_tracks = [client.get('/users/' + str(user['id']) + '/favorites') for user in users]
+			for track in user_tracks:
+				if hasattr(track, 'stream_url'):
+					tracks.append(dict(
+						id=track.id,
+						duration=track.duration,
+						title=track.title,
+						stream_url=track.stream_url,
+						user=track.user['username'],
+						date=track.created_at,
+						permalink=track.permalink_url,
+						favourited=user.username
+					))
+		with open('data/others_favourites.txt', 'w') as outfile:
+			json.dump({'tracks' : tracks}, outfile)
+		return jsonify({ 'tracks' : tracks })
+
+@app.route('/embedcode', methods=['GET', 'POST'])
 def embedcode():
-	track = request.args.get('track')
-	embed = client.get('/oembed', url=track)
-	return embed.html.encode('utf8')
+	if request.method == 'POST':
+		track_url = request.json['url']
+		url = request.json['url']
+		embed = client.get('/oembed', url=track_url)
+		return embed.html.encode('utf8')
+	return 'test'
 
 @app.route('/radio6')
 def radio6():
--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var app = angular.module('scloud', [ ]).filter('duration', function() {
+    var app = angular.module('scloud', [  ]).filter('duration', function() {
         return function(input) {
             var ms = input % 1000;
             s = (input - ms) / 1000;
@@ -13,19 +13,75 @@
         }
     });
 
-    app.controller('StreamController', [ '$http' , function( $http ){  
+    app.controller('StreamController', [ '$http', '$sce', function( $http, $sce ){  
+
+        this.pagination = '-date';
         var stream = this;
         stream.tracks = [];
-        this.pagination = '-date';
+        stream.embed_code = '';
+
         $http.get('/get_tracks').success(function(data){
             stream.tracks = data.tracks;
         });
 
         this.updatePagination = function(new_order){
-            this.pagination = '-' + new_order;
+            this.pagination = new_order;
         }
         this.play = function(stream_url){
             $http.post('/play_stream', { url : stream_url});
+        }
+        this.embed = function(stream_url){
+            $http.post('/embedcode', { url : stream_url}).success(function(data){
+                stream.embed_code = $sce.trustAsHtml(data)
+            });
+        }
+    } ] );
+
+    app.controller('FavouritesController', [ '$http', '$sce', function( $http, $sce ){  
+
+        this.pagination = '-date';
+        var stream = this;
+        stream.tracks = [];
+        stream.embed_code = '';
+
+        $http.get('/get_favourites').success(function(data){
+            stream.tracks = data.tracks;
+        });
+
+        this.updatePagination = function(new_order){
+            this.pagination = new_order;
+        }
+        this.play = function(stream_url){
+            $http.post('/play_stream', { url : stream_url});
+        }
+        this.embed = function(stream_url){
+            $http.post('/embedcode', { url : stream_url}).success(function(data){
+                stream.embed_code = $sce.trustAsHtml(data)
+            });
+        }
+    } ] );
+
+    app.controller('OthersController', [ '$http', '$sce', function( $http, $sce ){  
+
+        this.pagination = '-date';
+        var stream = this;
+        stream.tracks = [];
+        stream.embed_code = '';
+
+        $http.get('/get_others_favourites').success(function(data){
+            stream.tracks = data.tracks;
+        });
+
+        this.updatePagination = function(new_order){
+            this.pagination = new_order;
+        }
+        this.play = function(stream_url){
+            $http.post('/play_stream', { url : stream_url});
+        }
+        this.embed = function(stream_url){
+            $http.post('/embedcode', { url : stream_url}).success(function(data){
+                stream.embed_code = $sce.trustAsHtml(data)
+            });
         }
     } ] );
 
